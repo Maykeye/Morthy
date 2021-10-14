3e359993811127d05dd9e87563eb661cddd1ea91
mayk <maykeye@github.com>
2021-10-14 09:34:36 +0600

    Added EMIT and more tests
    Added EMIT, aka putc.
    Added more tests, now we can test stdout.

--- tests.py
+++ tests.py
@@ -7,10 +7,26 @@ def run_errcode_test(src):
         f.write(src)
     (res, out, err) = morth.run(filename, "nasm-ld-pipe")
     return res
-    
+def run_stdout_test(src):
+    filename = "out/_unittest.morth"
+    with open(filename, "w") as f:
+        f.write(f": run [ {src} ]\n")
+        f.write(f": main [ sentinel! run sentinel@ ]\n")
+    (res, out, err) = morth.run(filename, "nasm-ld-pipe")
+    return out.decode('utf-8')
+
 @pytest.mark.parametrize("num",[0,1,2,10,100,1000,12345,102030,1122444, 2**31, 2**63,2**64-1,0x10203040AABBCCDD])
 def test_exit0(num):
     assert run_errcode_test(f": main [ {num} exit ]") == num & 0xff
+
+def test_func_call():
+    assert run_errcode_test(
+        ": run [ run1 23 + ] \n"
+        ": run1 [ run2 100 run2 ] \n"
+        ": run2 [ ] \n"
+        ": main [ run exit ]"
+    ) == 123
+
 
 @pytest.mark.parametrize("num",[0,1,2,3,4,5,6,7,8,9])
 def test_hex_digit_digits(num):
@@ -37,3 +53,6 @@ def test_sentinel_unbalanced(s):
 ])
 def test_relop(s, res):
     assert run_errcode_test(f": main [ {s} exit ] ") == res
+
+def test_emit():
+    assert run_stdout_test(f"49 48 emit emit") == "01"
