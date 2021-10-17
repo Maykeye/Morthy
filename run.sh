996ad469c8f973b7202b6e21a0fd024db62b6db1
mayk <maykeye@github.com>
2021-10-18 00:31:43 +0600

    Added DWARF debug info.
    Now using gdb is less pita

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ set -e
 FILENAME=${1:-fizzbuzz.morth}
 BASE=`basename "${FILENAME}" .morth`
 python3 ./morth.py "$FILENAME"
-nasm -felf64 out/${BASE}.asm
+nasm -felf64 -g -F dwarf out/${BASE}.asm
 ld out/${BASE}.o -oout/${BASE}
 set +e
 ./out/${BASE}
